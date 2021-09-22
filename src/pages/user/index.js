97b31a6a59134c1c50dbf01e5c1b88023114a3e4
ahmdGeek <ahmadalymohamad@gmail.com>
2021-09-22 11:48:22 +0200

    working user dashboard before final refactor

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -6,68 +6,107 @@
  * User dashboard page
  * render cards of user subscribed courses
  */
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Head from 'next/head';
 import {
-  Grid, Link,
+  /* Grid, */ Link,
 } from '@material-ui/core';
 import {
-  getAllcourses, getUserDoc, protectPage,
+ /*  getAllcourses, getUserDoc, */ protectPage,
 } from '../../utils/server';
-import { CourseCard } from '../../components/CourseCard/Index';
+import {
+  apiPost,
+} from '../../utils/client';
+// import { CourseCard } from '../../components/CourseCard/Index';
+import CourseLibrary from '../../components/CourseLibrary';
+import { Loader } from '../../components/Loader';
+// import { useUser } from '../../customHooks';
 
-const Dashboard = ({ courses = [], user = {} }) => (
-  <>
-    <Head>
-      <title>
-        Dashboard
-      </title>
-    </Head>
-    {
-      courses?.length < 1
-        ? (
-          <>
-            <h1>
-              You are not enrolled in any course yet.
-            </h1>
-            <h3>
-              Click
-              &nbsp;
-              <Link href='/courses'>
-                <a>
-                  HERE
-                </a>
-              </Link>
-              &nbsp;
-              to see Courses Catalog
-            </h3>
-          </>
-        )
-        : (
-          <Grid container xs={12} spacing={3} alignContent="center">
-            {/* Courses cards */}
-            {
-              courses.map((course) => (
-                <CourseCard course={course} key={course?.id} user={user} />
-              ))
-            }
-          </Grid>
-        )
+const fetchUserCourses = () => (apiPost({ url: 'user/courses' }));
+
+const Dashboard = ({ /* courses = [], user = {} */ }) => {
+  // const { user } = useUser();
+
+  const [courses, setCourses] = useState([]);
+  const [state, setState] = useState('loading');
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(false);
+
+  useEffect(() => (fetchUserCourses()
+    .then(({ courses: resCourses }) => {
+      const coursesWithSubscription = resCourses
+        ?.map((crse) => ({ ...crse, subscribed: true }));
+      setCourses(coursesWithSubscription);
+      setState('fetched');
+    })
+    .catch(() => setState('error'))
+  ), []);
+
+  const renderContent = useCallback(() => {
+    switch (state) {
+      case 'loading':
+        return <Loader />;
+      case 'error':
+        return <h1>Something went wrong, please try again</h1>;
+      case 'fetched':
+        return (
+          courses?.length > 0
+            ? (
+              <CourseLibrary
+                courses={courses}
+              />
+            )
+            : (
+              <>
+                <h1>
+                  You are not enrolled in any course yet.
+                </h1>
+                <h3>
+                  Click
+                  &nbsp;
+                  <Link href='/courses'>
+                    <a>
+                      HERE
+                    </a>
+                  </Link>
+                  &nbsp;
+                  to see Courses Catalog
+                </h3>
+              </>
+            )
+        );
+      default:
+        return null;
     }
+  }, [courses, state]);
 
-  </>
-);
-
-const getSSProps = async (ctx, { userCookie }) => {
-  const courses = await getAllcourses({ uid: userCookie?.uid });
-  const userDoc = await getUserDoc({ uid: userCookie?.uid });
-  return {
-    props: {
-      courses,
-      user: userDoc,
-    },
-  };
+  return (
+    <>
+      <Head>
+        <title>
+          Dashboard
+        </title>
+      </Head>
+      {
+        renderContent()
+      }
+    </>
+  );
 };
+
+// const getSSProps = async (ctx, { userCookie }) => {
+//   const courses = await getAllcourses({ uid: userCookie?.uid });
+//   const userDoc = await getUserDoc({ uid: userCookie?.uid });
+//   console.log({ userDoc });
+//   console.log({ userCookie });
+//   return {
+//     props: {
+//       courses,
+//       user: userDoc,
+//     },
+//   };
+// };
+const getSSProps = () => ({ props: {} });
 
 export const getServerSideProps = protectPage(getSSProps);
 
