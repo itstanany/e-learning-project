5046db721cd5e2e6e513bdf50563104851f496b6
ahmdGeek <ahmadalymohamad@gmail.com>
2021-10-05 16:38:19 +0200

    fix, requireauth cookies check

--- src/utils/server/middlewares/requireAuth.js
+++ src/utils/server/middlewares/requireAuth.js
@@ -1,27 +1,31 @@
-import { withCookies } from '..';
-import { jwtVerify } from '../jwt';
+import { getUserCookie } from '..';
 
-const checkAuth = (req, res, { cookies, next }) => {
-  const userCookie = cookies.get('user');
-  try {
-    const user = jwtVerify({ token: userCookie });
+/**
+ * Check for auth of user
+ * Attach "userCookie" to request body
+ * @param {object} req NextJs Request object
+ * @param {object} res NextJs response Object
+ * @param {function} next - next middleware in middleware chain
+ * @returns execute next function if user is authenticated
+ * @returns 401 response with error message of not authenticated
+ */
+const checkAuth = (req, res, next) => {
+  const userCookie = getUserCookie(req, res);
+  if (userCookie) {
+    // const user = jwtVerify({ token: userCookie });
     // avoid empty string req.body
     req.body = {
       ...req.body,
-      user,
       // rename user to userCookie,
       //  ... so avoid the possibility of "user" field in body ibject send by the client
-      userCookie: user,
+      userCookie,
     };
-    next();
-  } catch (error) {
-    console.log('requireAuth error');
-    console.log({ error });
-    res.status(401).json({ error: 'not authenticated' });
+    return next();
   }
+  return res.status(401).json({ error: 'not authenticated' });
 };
 
-const requireAuth = withCookies(checkAuth);
+const requireAuth = checkAuth;
 
 export {
   requireAuth,
